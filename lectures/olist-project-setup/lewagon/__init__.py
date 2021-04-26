eee6449dd09e45be0916df4a5d0157f6804232f9
Trouni Tiet <trouni@gmail.com>
2021-04-26 11:48:09 +0900

    Update comment with correct python version

--- lectures/olist-project-setup/lewagon/__init__.py
+++ lectures/olist-project-setup/lewagon/__init__.py
@@ -1,3 +1,3 @@
-# __init__.py is not necessary anymore 3.something
+# __init__.py is not necessary anymore for python 3.3+
 # but it will get run whenever you import the package
 print("Initializing the lewagon package...")
